5262eeec69b5ae8a94c84fcdcf96465cc7e6ec5c
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-04 21:49:55 -0600

    Refine comments

--- typescript/reflection/reflect-metadata/basic-usage.ts
+++ typescript/reflection/reflect-metadata/basic-usage.ts
@@ -3,10 +3,12 @@ import "reflect-metadata";
 class BasicUsageDemoClass {
     constructor() {
         // Explicitly define some metadata
+        // key, value, target, propertyKey
         Reflect.defineMetadata("foo1", "bar1", this, "baz");
     }
 
     // Define metadata via a decorator
+    // key, value
     @Reflect.metadata("foo2", "bar2")
     public baz() {
         // do nothing
@@ -15,6 +17,7 @@ class BasicUsageDemoClass {
 
 const basicUsageDemo = new BasicUsageDemoClass();
 
+// key, target, propertyKey
 console.log(Reflect.getMetadata("foo1", basicUsageDemo, "baz"));
 // bar1
 console.log(Reflect.getMetadata("foo2", basicUsageDemo, "baz"));
