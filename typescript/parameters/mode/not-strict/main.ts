35b0edb9b1e26e578880a0449253603bc9f19613
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-10 08:57:39 -0600

    Create mode examples

---
+++ typescript/parameters/mode/not-strict/main.ts
@@ -0,0 +1,20 @@
+function DecoratedParameter(
+    target: any,
+    propertyKey: string,
+    parameterIndex: number
+) {
+    console.log("this.constructor.name:", this.constructor.name);
+    console.log("target.constructor.name:", target.constructor.name);
+    console.log(`this ${this === target ? '=' : '!'}== target`);
+    target.stringProperty = "changed in decorator";
+}
+
+class NotStrictModeClass {
+    public stringProperty: string;
+    constructor() {
+        console.log(this.stringProperty);
+    }
+    public foo(@DecoratedParameter bar: any) { }
+}
+
+const notStrictModeDemo = new NotStrictModeClass();
