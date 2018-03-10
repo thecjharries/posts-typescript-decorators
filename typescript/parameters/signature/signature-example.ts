ce57338607cc1448b0d8aa04ed0d8590cfc113b5
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-10 10:57:47 -0600

    Simplify signature example

--- typescript/parameters/signature/signature-example.ts
+++ typescript/parameters/signature/signature-example.ts
@@ -2,14 +2,8 @@ function DecoratedParameter(
     target: any,
     propertyKey: string,
     parameterIndex: number
-) {
-    console.log(target);
-    console.log(propertyKey);
-    console.log(parameterIndex);
-}
+) { }
 
 class TargetDemo {
-    public foo(baz: any, @DecoratedParameter bar: any) {
-        console.log("Class method foo");
-    }
+    public foo(baz: any, @DecoratedParameter bar: any) { }
 }
