7112d59f7540735f297635b52b85f2d2f9396708
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-10 11:26:05 -0600

    Use double quotes

--- typescript/reflection/usage/ValidateRange.ts
+++ typescript/reflection/usage/ValidateRange.ts
@@ -28,7 +28,7 @@ export function ValidateRange(
             const value = args[key];
             // Throw error if outside range
             if (value < range[0] || value > range[1]) {
-                throw new Error('Value outside of range');
+                throw new Error("Value outside of range");
             }
         }
         // Actually call the function
