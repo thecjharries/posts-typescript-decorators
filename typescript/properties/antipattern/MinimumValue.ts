9df7260e68ad7de3125c8073917c1eeb73ed1528
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-10 16:16:53 -0600

    Comment MinimumValue

--- typescript/properties/antipattern/MinimumValue.ts
+++ typescript/properties/antipattern/MinimumValue.ts
@@ -1,6 +1,8 @@
 export function MinimumValue(min: number = 0) {
+    // Scope the value to be reused
     let value: number;
     return (target: any, propertyKey: string | symbol) => {
+        // Store the definition result
         const update = Reflect.defineProperty(
             target,
             propertyKey,
@@ -8,9 +10,11 @@ export function MinimumValue(min: number = 0) {
                 configurable: true,
                 enumerable: true,
                 get: () => {
+                    // Return the scoped value
                     return value;
                 },
                 set: (newValue: number) => {
+                    // Update the scoped value with max(newValue, min)
                     value = (
                         newValue >= min
                             ? newValue
@@ -19,7 +23,9 @@ export function MinimumValue(min: number = 0) {
                 }
             },
         );
+        // If the update failed, something went wrong
         if (!update) {
+            // Kill everything
             throw new Error("Unable to update property");
         }
     }
